264ebff476bc4391af0d86c7805f9d2c20110957
ulemumwenye <113883746+ulemumwenye@users.noreply.github.com>
2023-05-22 14:22:50 +0200

    Update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,28 @@
-#!/usr/bin/python3
-"""script using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
 import requests as r
 import sys
 
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
-    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
-    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
-#    print(to_do)
-    completed = [title.get("title") for title in to_do if
-                 title.get('completed') is True]
-    print(completed)
-    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
-                                                          len(completed),
-                                                          len(to_do)))
-    [print("\t {}".format(title)) for title in completed]
+    user_id = sys.argv[1]
+
+    try:
+        user_response = r.get(url + 'users/{}'.format(user_id), verify=False)
+        user_response.raise_for_status()
+        user_data = user_response.json()
+        user_name = user_data['name']
+    except (r.RequestException, ValueError, KeyError) as e:
+        print(f"Error: Failed to retrieve user information. {str(e)}")
+        sys.exit(1)
+
+    try:
+        todo_response = r.get(url + 'todos', params={'userId': user_id}, verify=False)
+        todo_response.raise_for_status()
+        todo_data = todo_response.json()
+        completed_tasks = [task for task in todo_data if task['completed']]
+    except (r.RequestException, ValueError, KeyError) as e:
+        print(f"Error: Failed to retrieve TODO list. {str(e)}")
+        sys.exit(1)
+
+    print(f"Employee {user_name} is done with tasks({len(completed_tasks)}/{len(todo_data)}):")
+    for task in completed_tasks:
+        print("\t" + task['title'])
