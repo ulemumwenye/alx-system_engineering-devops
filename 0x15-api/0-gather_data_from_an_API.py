b1375dd5eb8d12d0632522a1ff032bd5d2b8891a
ulemumwenye <113883746+ulemumwenye@users.noreply.github.com>
2023-05-22 14:25:40 +0200

    Update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,6 +13,6 @@ if __name__ == '__main__':
                  title.get('completed') is True]
     print(completed)
     print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
- len(completed),
- len(to_do)))
+                                                          len(completed),
+                                                          len(to_do)))
     [print("\t {}".format(title)) for title in completed]
