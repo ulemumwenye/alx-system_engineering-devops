aaa2548c8f6cdccb3e6f7cf6c19eecf6e57d07ee
ulemumwenye <113883746+ulemumwenye@users.noreply.github.com>
2023-05-24 10:50:04 +0200

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,19 +2,22 @@ import csv
 import json
 import requests
 import sys
+import urllib3
 
 base_url = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == "__main__":
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
     user_id = sys.argv[1]
 
     user_url = '{}/users?id={}'.format(base_url, user_id)
-    response = requests.get(user_url)
+    response = requests.get(user_url, verify=False)
     user_data = json.loads(response.text)
     username = user_data[0].get('username')
 
     tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
-    response = requests.get(tasks_url)
+    response = requests.get(tasks_url, verify=False)
     tasks = json.loads(response.text)
 
     file_name = '{}.csv'.format(user_id)
