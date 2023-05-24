5e370d9c72d30064e00dd20b3747713f7477690f
ulemumwenye <113883746+ulemumwenye@users.noreply.github.com>
2023-05-24 11:04:09 +0200

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""
+This script retrieves user data and tasks from an API and exports the data to a CSV file.
+"""
+
 import csv
 import json
 import requests
@@ -9,23 +14,28 @@ base_url = 'https://jsonplaceholder.typicode.com'
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
 
+    # Retrieve user ID from command-line argument
     user_id = sys.argv[1]
 
+    # Retrieve user data
     user_url = '{}/users?id={}'.format(base_url, user_id)
     response = requests.get(user_url, verify=False)
     user_data = json.loads(response.text)
     username = user_data[0].get('username')
 
+    # Retrieve tasks
     tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
     response = requests.get(tasks_url, verify=False)
     tasks = json.loads(response.text)
 
+    # Create and write to CSV file
     file_name = '{}.csv'.format(user_id)
     with open(file_name, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
         for task in tasks:
-            completed_status = "Yes" if task.get('completed') else "No"
+            completed_status = True if task.get('completed') else False
             writer.writerow([user_id, username, completed_status, task.get('title')])
 
+    # Print success message
     print("Data exported to {}.csv".format(user_id))
