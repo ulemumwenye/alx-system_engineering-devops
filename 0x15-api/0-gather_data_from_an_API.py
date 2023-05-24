fdbfa88f8f1dda2364ec2dfe332810eeb157a75a
ulemumwenye <113883746+ulemumwenye@users.noreply.github.com>
2023-05-24 10:13:35 +0200

    Update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,64 +1,51 @@
 #!/usr/bin/python3
 """
-Script that fetches info about a given employee using an APIs.
+Script that fetches information about a given employee's ID using an API.
 """
 
 import json
 import requests
 import sys
 
-
-def get_employee_name(employee_id):
-    """
-    Retrieves the name of the employee with the given ID from the API.
-
-    Args:
-        employee_id: ID of the employee.
-
-    Returns:
-        Name of the employee.
-    """
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    response = requests.get(url, verify=False)
-    data = response.json()
-    return data.get("name")
-
-
-def get_employee_todo_progress(employee_id):
-    """
-    Retrieves the TODO list progress for the employee with the given ID from the API.
-
-    Args:
-        employee_id: ID of the employee.
-
-    Returns:
-        A tuple containing the number of completed tasks and the total number of tasks.
-    """
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
-    response = requests.get(url, verify=False)
-    data = response.json()
-
-    total_tasks = len(data)
-    completed_tasks = sum(task.get("completed") for task in data)
-
-    return completed_tasks, total_tasks
-
+base_url = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == "__main__":
-    # Retrieve employee ID from command-line argument
-    employee_id = sys.argv[1]
+    """
+    Retrieves information about a given employee's ID and displays their completed tasks.
+    """
+    user_id = sys.argv[1]
 
-    # Get the name of the employee
-    employee_name = get_employee_name(employee_id)
+    # Get user info e.g https://jsonplaceholder.typicode.com/users/1/
+    user_url = '{}/users?id={}'.format(base_url, user_id)
 
-    # Get the TODO list progress for the employee
-    completed_tasks, total_tasks = get_employee_todo_progress(employee_id)
+    # Get info from API
+    response = requests.get(user_url)
+    data = response.json()
+
+    # Extract user data, in this case, the name of the employee
+    name = data[0].get('name')
+
+    # Get user info about todo tasks e.g https://jsonplaceholder.typicode.com/users/1/todos
+    tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
+
+    # Get info from API
+    response = requests.get(tasks_url)
+    tasks = response.json()
+
+    # Initialize completed count as 0 and find the total number of tasks
+    completed = 0
+    total_tasks = len(tasks)
+
+    # Initialize an empty list for completed tasks
+    completed_tasks = []
+
+    # Loop through tasks counting the number of completed tasks
+    for task in tasks:
+        if task.get('completed'):
+            completed_tasks.append(task)
+            completed += 1
 
     # Print the output in the required format
-    print("Employee {} is done with tasks ({}/{}):".format(employee_name, completed_tasks, total_tasks))
-    for i in range(total_tasks):
-        url = "https://jsonplaceholder.typicode.com/todos/{}".format(i + 1)
-        response = requests.get(url, verify=False)
-        task = response.json()
-        if task.get("completed"):
-            print("\t{}".format(task.get("title")))
+    print("Employee {} is done with tasks ({}/{}):".format(name, completed, total_tasks))
+    for task in completed_tasks:
+        print("\t{}".format(task.get('title')))
